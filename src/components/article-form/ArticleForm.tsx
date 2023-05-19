6e26f19626f54d84cf718ea9031edc1e069af287
YanPavlukovich <filin022@gmail.com>
2023-05-19 22:40:24 +0300

    fix add loader on edit article добавляем лоадер при загрузке данных по редактированию страницы, дополнительно создаем новый useEffect для проверки наличия article(defaultValues.article), который мы упростили в EditArticlePage, включаем лоадер на true

--- src/components/article-form/ArticleForm.tsx
+++ src/components/article-form/ArticleForm.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { DeepPartial, useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup/dist/yup";
 
@@ -6,6 +6,7 @@ import { Input } from "../input/Input";
 import { FormValues, articleFormValidation } from "./ArticleFormValidation";
 import { ArticleFormTags } from "./ArticleFormTags";
 import { ArticleFormStyle } from "./article-form.style";
+import { CircularProgress } from "@mui/material";
 
 type Props = {
   title: string;
@@ -13,11 +14,12 @@ type Props = {
   errors?: {
     [key: string]: string;
   };
-  defaultValues?: DeepPartial<FormValues>;
+  defaultValues?: DeepPartial<FormValues> | null;
 };
 
 export const ArticleForm = (props: Props) => {
   const { title, submitHandler, errors: propErrors, defaultValues } = props;
+  const [isLoading, setIsLoading] = useState(true);
 
   const {
     register,
@@ -30,7 +32,7 @@ export const ArticleForm = (props: Props) => {
   } = useForm<FormValues>({
     mode: "onChange",
     resolver: yupResolver(articleFormValidation),
-    defaultValues,
+    defaultValues: defaultValues || {},
   });
 
   const watchedValues = watch();
@@ -47,43 +49,57 @@ export const ArticleForm = (props: Props) => {
   }, [propErrors]);
 
   useEffect(() => {
-    reset(defaultValues);
+    if (defaultValues) {
+      reset(defaultValues);
+      setIsLoading(false);
+    } else {
+      setIsLoading(true);
+    }
   }, [defaultValues]);
 
   return (
-    <ArticleFormStyle>
-      <h2 className={"title"}>{title}</h2>
+    <>
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        <ArticleFormStyle>
+          <h2 className={"title"}>{title}</h2>
 
-      <form style={{ marginTop: 20 }} onSubmit={handleSubmit(submitHandler)}>
-        <Input
-          name={"articleTitle"}
-          register={register}
-          label={watchedValues.articleTitle ? "" : "Title"}
-          error={errors.articleTitle?.message as string}
-        />
-        <Input
-          name={"description"}
-          register={register}
-          label={watchedValues.description ? "" : "Short description"}
-          error={errors.description?.message as string}
-        />
-        <Input
-          name={"text"}
-          register={register}
-          label={watchedValues.text ? "" : "Text"}
-          type={"textarea"}
-          error={errors.text?.message as string}
-        />
-        <ArticleFormTags
-          control={control}
-          register={register}
-          errors={errors}
-          tagError={errors?.tags?.message as string}
-        />
-        <button type={"submit"} className="formSubmit">
-          Send
-        </button>
-      </form>
-    </ArticleFormStyle>
+          <form
+            style={{ marginTop: 20 }}
+            onSubmit={handleSubmit(submitHandler)}
+          >
+            <Input
+              name={"articleTitle"}
+              register={register}
+              label={watchedValues.articleTitle ? "" : "Title"}
+              error={errors.articleTitle?.message as string}
+            />
+            <Input
+              name={"description"}
+              register={register}
+              label={watchedValues.description ? "" : "Short description"}
+              error={errors.description?.message as string}
+            />
+            <Input
+              name={"text"}
+              register={register}
+              label={watchedValues.text ? "" : "Text"}
+              type={"textarea"}
+              error={errors.text?.message as string}
+            />
+            <ArticleFormTags
+              control={control}
+              register={register}
+              errors={errors}
+              tagError={errors?.tags?.message as string}
+            />
+            <button type={"submit"} className="formSubmit">
+              Send
+            </button>
+          </form>
+        </ArticleFormStyle>
+      )}
+    </>
   );
 };
